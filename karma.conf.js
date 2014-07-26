e21d55742bd8aac8c35d772bd36aa956c48f03b1
Paul Everitt <paul@agendaless.com>
2014-07-26 09:44:08 +0200

    Update karma conf. Fix dist.

--- karma.conf.js
+++ karma.conf.js
@@ -7,79 +7,40 @@ module.exports = function (config) {
 
       // list of files / patterns to load in the browser
       files: [
-        'bower_components/angular/angular.js',
-        'bower_components/angular-route/angular-route.js',
-        'bower_components/angular-resource/angular-resource.js',
-        'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
-        'bower_components/angular-mocks/angular-mocks.js',
-        'bower_components/angular-cookies/angular-cookies.js',
-        'bower_components/lodash/dist/lodash.js',
+        "bower_components/angular/angular.js",
+        "bower_components/angular-mocks/angular-mocks.js",
+        "bower_components/angular-ui-router/release/angular-ui-router.js",
+        "bower_components/lodash/dist/lodash.js",
 
-        'dist/js/ngtemplates.js',
-        'src/app/app.js',
-        'src/mockRest.js',
-        'src/common/interceptors.js',
-
-        'src/and/and.js',
-        'src/and/tests/*specs.js',
-        'src/or/or.js',
-        'src/or/tests/*specs.js',
-        'src/absolute/absolute.js',
-        'src/absolute/tests/*specs.js',
-        'src/exclude/exclude.js',
-        'src/exclude/tests/*specs.js',
-        'src/relative/relative.js',
-        'src/relative/tests/*specs.js',
-        'src/withn/withn.js',
-        'src/withn/tests/*specs.js',
-        'src/toolkit/toolkit.js',
-        'src/toolkit/tests/*specs.js',
-
-        'src/project/resources.js',
-        'src/project/tests/*.specs.js',
-
-        'src/completed/completed.js',
-        'src/completed/tests/*.specs.js',
-        'src/tooltip/tooltip.js',
-        'src/tooltip/tests/*specs.js',
-        'src/removenode/removenode.js',
-        'src/removenode/tests/*specs.js',
-        'src/genomic/tests/*specs.js',
-        'src/genomic/genomic.js',
-
-        'src/dashboard/dashboard.js',
-        'src/dashboard/tests/*specs.js',
-        'src/query/query.js',
-        'src/query/tests/*specs.js',
-
-        'src/clobject/clobject.js',
-        'src/clobject/tests/*specs.js'
+        "src/*/*.js",
+        "dist/js/ngtemplates.js",
+        "src/*/tests/*specs.js"
       ],
 
       // base path, that will be used to resolve files and exclude
       logLevel: config.LOG_DEBUG,
 
       // frameworks to use
-      frameworks: ['jasmine'],
+      frameworks: ["jasmine"],
 
-      browsers: ['PhantomJS'],
-//            browsers: ['Chrome'],
+      browsers: ["PhantomJS"],
+//            browsers: ["Chrome"],
 
       // coverage reporter generates the coverage
-      reporters: ['progress', 'junit', 'coverage'],
+      reporters: ["progress", "junit", "coverage"],
 
       preprocessors: {
-        'src/*/*.js': ['coverage']
+        "src/*/*.js": ["coverage"]
       },
 
       junitReporter: {
-        outputFile: 'karma-test-results.xml'
+        outputFile: "karma-test-results.xml"
       },
 
       coverageReporter: {
-        type: 'cobertura',
-        dir: 'coverage/',
-        file: 'coverage.xml'
+        type: "cobertura",
+        dir: "coverage/",
+        file: "coverage.xml"
       }
 
 
