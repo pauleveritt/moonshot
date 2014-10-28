73e32094b579133a4766e415dbc7a365d54a0721
Paul Everitt <paul@agendaless.com>
2014-10-28 13:43:14 -0400

    Yay, looks like commit works.

--- sqladdcontent/tutorial/__init__.py
+++ sqladdcontent/tutorial/__init__.py
@@ -1,17 +1,11 @@
 from pyramid.config import Configurator
 
-from sqlalchemy import engine_from_config
-
 from .models.sqltraversal import (
     root_factory
-    )
+)
 
 
 def main(global_config, **settings):
-    engine = engine_from_config(settings, 'sqlalchemy.')
-    DBSession.configure(bind=engine)
-    Base.metadata.bind = engine
-
     config = Configurator(settings=settings,
                           root_factory=root_factory)
     config.include('pyramid_tm')
